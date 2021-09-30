7f7892537204bfd02f9f4e123301eef41a740902
Stefaan De Vylder <stefaan.devylder@gmail.com>
2021-09-30 17:59:17 +0200

    Added Dockerfile and orderform test

--- src/components/orders/NewOrderForm.test.js
+++ src/components/orders/NewOrderForm.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent, act } from "@testing-library/react";
 import NewOrderForm from "./NewOrderForm";
 
 describe('NewOrderForm Test Suits', () => {
-    test('Add new product and adds it to the list', async () => {
+    test('Add new product and save the order', async () => {
         const addOrderHandler = jest.fn();
         await act( async() => render(<NewOrderForm onAddOrder={addOrderHandler}/>));
 
@@ -11,11 +11,15 @@ describe('NewOrderForm Test Suits', () => {
 
         fireEvent.change(amountEl, { target: { value: '1' } });
         fireEvent.click(buttonEl);
-        
-        const listItems = await screen.findAllByRole('listitem');
 
-        expect(listItems).toHaveLength(1);
+        const idEl = screen.getByPlaceholderText("Id");
+        const customerIdEl = screen.getByPlaceholderText("Customer ID");
+        const saveButtonEl = screen.getByText("Slaag order op");
+
+        fireEvent.change(idEl, { target: { value: '4'}});
+        fireEvent.change(customerIdEl, { target: { value: '1'}});
+        fireEvent.click(saveButtonEl);
+
+        expect(addOrderHandler.mock.calls.length).toBe(1);
     });
-
-    
 }); 
